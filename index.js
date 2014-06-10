72c4c2b17f2ee559bf1476069b0151b621a0f0db
Bendik Solheim <bendik.solheim@bekk.no>
2014-06-10 18:56:41 +0200

    Added backend start

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var fs = require('fs');
+
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/twitter', function(req, res) {
+	res.end("lol");
+});
+
+app.listen(1337);
