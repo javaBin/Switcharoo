8dcfdaef3afef19fcc63508e5dc3c7c022d6ee53
Bendik Solheim <bendik.solheim@bekk.no>
2014-06-15 13:23:43 +0200

    Added sample configuration

---
+++ configuration_example.js
@@ -0,0 +1,14 @@
+/*
+	Copy me into the file `configuration.js` and fill in the blanks
+*/
+
+var config = {
+	twitter: {
+		consumer_key: '',
+		consumer_secret: '',
+		access_token: '',
+		access_token_secret: ''
+	}
+};
+
+module.exports = config;
