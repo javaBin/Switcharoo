6511b4d6b30780e0bce4b1ca581b2e62ea470512
Bendik Solheim <bendik.solheim@bekk.no>
2014-07-28 21:48:17 +0200

    Now finds current talks and sorts on room. Will probably choke badly on lightning talks

--- program.js
+++ program.js
@@ -1,10 +1,13 @@
 var cron = require('cron').CronJob;
 var config = require('./configuration').program;
 var request = require('request');
+var moment = require('moment');
 
 var cronPattern = config.cronPattern || '0 */1 * * * *';
 
 var current_program = null;
+
+var date = moment("2013-09-11T15:40:00Z");
 
 var job = new cron(cronPattern, getProgram);
 
@@ -18,9 +21,12 @@ function getProgram(complete) {
 		if (!Array.isArray(body))
 			return console.error("Response from \"" + config.url + "\" was not an array: " + body);
 
-		current_program = body.filter(function(talk) {
-			return talk.room === 'Room 4';
-		});
+		current_program = body
+			.filter(function(talk) {
+				return moment(talk.start).isBefore(date) && moment(talk.stop).isAfter(date);
+			}).sort(function(a, b) {
+				return a.room > b.room;
+			});
 
 		if (complete)
 			complete();
