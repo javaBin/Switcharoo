b280418ccd058915e4b350e39edddc5827be861e
Bendik Solheim <bendik@solheim.im>
2017-01-04 21:31:54 +0100

    Newline after each css rule

--- backend/server/services/css-generator.js
+++ backend/server/services/css-generator.js
@@ -3,7 +3,7 @@ const Css = require('../models').Css;
 function css(res) {
     Css.findAll().then(rules => {
         const styleSheet = rules.map(rule => {
-            return `${rule.selector} { ${rule.property}: ${rule.value}; }`;
+            return `${rule.selector} { ${rule.property}: ${rule.value}; }\n`;
         }).join('');
 
         res.set('Content-Type', 'text/css; charset=UTF-8');
