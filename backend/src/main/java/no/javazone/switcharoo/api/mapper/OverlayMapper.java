c94739be53881e04a875b28441b1607792aa9dcf
Bendik Solheim <bendik@solheim.im>
2018-04-13 23:12:31 +0200

    Don’t expose overlay if disabled or not set

--- backend/src/main/java/no/javazone/switcharoo/api/mapper/OverlayMapper.java
+++ backend/src/main/java/no/javazone/switcharoo/api/mapper/OverlayMapper.java
@@ -6,7 +6,11 @@ import no.javazone.switcharoo.dao.model.DBOverlay;
 public class OverlayMapper {
 
     public static Overlay fromDb(DBOverlay overlay) {
-        return new Overlay(overlay.enabled, overlay.image, overlay.placement, overlay.width, overlay.height);
+        if (overlay.image == null || overlay.image.equals("") || !overlay.enabled) {
+            return null;
+        } else {
+            return new Overlay(overlay.enabled, overlay.image, overlay.placement, overlay.width, overlay.height);
+        }
     }
 
     public static DBOverlay toDb(Overlay overlay) {
