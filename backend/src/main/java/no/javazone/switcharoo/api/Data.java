c1b67ff7d4f7de140c9fed724ee5fd6ad8bdf6db
Bendik Solheim <bendik@solheim.im>
2018-05-28 21:04:13 +0200

    Remove unknown slide types in backend

--- backend/src/main/java/no/javazone/switcharoo/api/Data.java
+++ backend/src/main/java/no/javazone/switcharoo/api/Data.java
@@ -8,6 +8,7 @@ import no.javazone.switcharoo.api.mapper.OverlayMapper;
 import no.javazone.switcharoo.api.mapper.SlideMapper;
 import no.javazone.switcharoo.api.model.Overlay;
 import no.javazone.switcharoo.api.model.PublicData;
+import no.javazone.switcharoo.api.model.Slide;
 import no.javazone.switcharoo.api.model.Tweet;
 import no.javazone.switcharoo.api.model.TwitterSlide;
 import no.javazone.switcharoo.dao.ConferenceDao;
@@ -46,7 +47,7 @@ public class Data implements HttpService {
             Application.setConference(req, conferences);
             List<Object> data = List.empty();
             long conference = req.attribute("conference");
-            data = data.appendAll(slides.listVisible(conference).map(SlideMapper::fromDb));
+            data = data.appendAll(slides.listVisible(conference).map(SlideMapper::fromDb).filter(this::knownTypes));
             if (isTwitterEnabled(conference)) {
                 List<Tweet> t = twitter.tweets(conference);
                 if (t.size() > 0) {
@@ -59,6 +60,10 @@ public class Data implements HttpService {
             res.type("application/json");
             return gson.toJson(new PublicData(data, overlay));
         });
+    }
+
+    private boolean knownTypes(Slide slide) {
+        return "text".equals(slide.type) || "image".equals(slide.type) || "video".equals(slide.type) || "tweets".equals(slide.type) || "program".equals(slide.type);
     }
 
     private boolean isTwitterEnabled(long conference) {
